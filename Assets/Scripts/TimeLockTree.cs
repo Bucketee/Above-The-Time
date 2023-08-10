59c08d2b78bf47ef61b2b9a9ae5654df4d4ee826
jhoh0518 <129178006+jhoh0518@users.noreply.github.com>
2023-08-10 22:06:08 +0900

    TimeLockTree 1

---
+++ Assets/Scripts/TimeLockTree.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum TreeState
+{
+    seed,
+    sprout,
+    tree
+}
+
+public class TimeLockTree : TimeLockObject
+{
+    [SerializeField] private Sprite[] treeSprites;
+    
+    private SpriteRenderer spriteRenderer;
+    private TreeState state;
+    private bool timeLocked => TimeLocked;
+    private bool canTransform;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        state = TreeState.seed;
+        canTransform = true;
+    }
+
+    private void Update()
+    {
+        if (timeLocked && canTransform)
+        {
+            var scroll = Input.GetAxis("Mouse ScrollWheel");
+            if(scroll > 0.5f && (int)state < 2) 
+            { 
+                canTransform = false;
+                state = (TreeState)((int)state + 1);
+                StartCoroutine(TreeGrowMotion());
+            }
+            else if (scroll < -0.5f && (int)state > 0)
+            {
+                canTransform = false;
+                state = (TreeState)((int)state - 1);
+                StartCoroutine(TreeReverseMotion());
+            }
+        }
+
+
+    }
+
+
+    IEnumerator TreeGrowMotion()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            transform.localScale = transform.localScale + new Vector3(0f, 0.01f, 0f);
+            transform.position += new Vector3(0f, 0.5f/100, 0f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        spriteRenderer.sprite = treeSprites[(int)state];
+        canTransform = true;
+    }
+
+    IEnumerator TreeReverseMotion()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            transform.localScale = transform.localScale - new Vector3(0f, 0.01f, 0f);
+            transform.position += new Vector3(0f, -0.5f / 100, 0f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        spriteRenderer.sprite = treeSprites[(int)state];
+        canTransform = true;
+    }
+}
